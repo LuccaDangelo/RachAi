980eb21a8c3cd5da549c8237eb2d3b81b1bbc74f
RafaelCoutinhoLima <rafaelcoutinholima12345@gmail.com>
2025-09-19 11:46:05 -0300

    fiz um feat: cria view e template inicial para listagem de grupos 'fakes'

--- Create_Group/views.py
+++ Create_Group/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def list_groups(request):
+ 
+    fake_groups_data = [
+        {'nome': 'Viagem para a Praia', 'descricao': 'Despesas do feriado.'},
+        {'nome': 'Contas do Apartamento', 'descricao': 'Aluguel, internet, luz.'},
+        {'nome': 'Jantar de Aniversário', 'descricao': 'Comemoração do Rafael.'},
+    ]
+    context = {
+        'page_title': 'Meus Grupos (Dados de Teste)',
+        'lista_de_grupos': fake_groups_data,
+    }
+    return render(request, 'Create_Group/group_list.html', context)
