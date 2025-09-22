7fee57f698e95b75f914027b179c44fc5c9f7549
RafaelCoutinhoLima <rafaelcoutinholima12345@gmail.com>
2025-09-22 14:36:19 -0300

    ajustando os erros das views e urls

--- Create_Group/views.py
+++ Create_Group/views.py
@@ -1,19 +1,7 @@
-from django.shortcuts import render,redirect
-from .models import Group
-from .forms import GroupForm
-
+from django.shortcuts import render
+from django.http import HttpResponse
 # Create your views here.
 def create_group(request):
-    '''View para criar um novo grupo de despesas'''
-    if request.method == 'POST':
-        form = GroupForm(request.POST)
-        if form.is_valid():
-            group = form.save()
-            '''Redireciona para a página de detalhes do grupo após a criação'''
-            return redirect('group_detail', group_id=group.id)
-    else:
-        '''Se a requisição não for POST, exibe o formulário vazio'''
-        form = GroupForm()
-
-    '''Renderiza o template 'create_group.html' passando o formulário'''
-    return render(request, 'create_group/create_group.html', {'form': form})
+    # Esta é uma função de teste. Ela retorna uma resposta HTTP simples.
+    # Mais tarde, você irá adicionar a lógica real aqui.
+    return HttpResponse("<h1>Esta é a página para criar um grupo.</h1>")
