a939fc01b0cb7e3a468f0f7dbf51197cafc04b14
RafaelCoutinhoLima <rafaelcoutinholima12345@gmail.com>
2025-09-22 14:56:15 -0300

    ajeitando outros erros da view,models

--- Create_Group/views.py
+++ Create_Group/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from Create_Group.forms import GroupForm
 # Create your views here.
 def create_group(request):
-    # Esta é uma função de teste. Ela retorna uma resposta HTTP simples.
-    # Mais tarde, você irá adicionar a lógica real aqui.
-    return HttpResponse("<h1>Esta é a página para criar um grupo.</h1>")
+    form=GroupForm()
+    return render(request,'create_group.html',{'form':form})
