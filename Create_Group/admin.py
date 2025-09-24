7850edb7b78e4e0ef783ca5471f202afc196b51e
joaoeaa <joaoeduardo.andrade03@gmail.com>
2025-09-24 11:02:46 -0300

    Add models na admin

--- Create_Group/admin.py
+++ Create_Group/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from Create_Group.models import Expense, Group, Participant
+
 # Register your models here.
+admin.site.register(Group)
+admin.site.register(Participant)
+admin.site.register(Expense)
