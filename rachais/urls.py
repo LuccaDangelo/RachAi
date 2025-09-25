aaea6c39898e20194696c9d77f661273e656f3ad
mmarc3lo <marceloamenezes07@gmail.com>
2025-09-25 15:16:15 -0300

    Url home funcionando

--- rachais/urls.py
+++ rachais/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("groups/", views.group_list, name="group_list"),
     path("groups/create/", views.create_group, name="create_group"),
     path("groups/<int:group_id>/", views.group_detail, name="group_detail"),
+    path("home/", views.home, name="home"),
+
 ]
