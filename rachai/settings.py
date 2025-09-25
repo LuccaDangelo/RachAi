6ebdf6a4529e97658319f8c3f46b876df73b6860
mmarc3lo <marceloamenezes07@gmail.com>
2025-09-24 23:25:36 -0300

    adidionando arquivo tela home + logo

--- rachai/settings.py
+++ rachai/settings.py
@@ -83,7 +83,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'assets'
+    BASE_DIR / 'assets',
     ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
