f9b214dd8087583497e158e4b868810fc5166153
RafaelCoutinhoLima <rafaelcoutinholima12345@gmail.com>
2025-09-27 09:35:19 -0300

    ajustei a settings para a azure

--- rachai/settings.py
+++ rachai/settings.py
@@ -4,16 +4,56 @@ Django settings for rachai project.
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
-# Base dirs
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Security
-SECRET_KEY = 'django-insecure-+(oe-3=($r76(x6@=0+)x*$lya)5jw)e@3!6!#zf*0@dg##)8_'
-DEBUG = True
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+load_dotenv(BASE_DIR / '.env')
 
-# Apps
+
+TARGET_ENV = os.getenv('TARGET_ENV')
+
+NOT_PROD = not (TARGET_ENV and TARGET_ENV.lower().startswith('prod'))
+
+if NOT_PROD:
+
+    DEBUG = True
+
+    SECRET_KEY = 'django-insecure-+(oe-3=($r76(x6@=0+)x*$lya)5jw)e@3!6!#zf*0@dg##)8_'
+
+    ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+   
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
+    ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+    CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(' ')
+
+    SECURE_SSL_REDIRECT = os.getenv('SECURE_SSL_REDIRECT', '0').lower() in ['true', 't', '1']
+
+    if SECURE_SSL_REDIRECT:
+        SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DBNAME'),
+            'HOST': os.environ.get('DBHOST'),
+            'USER': os.environ.get('DBUSER'),
+            'PASSWORD': os.environ.get('DBPASS'),
+            'OPTIONS': {'sslmode': 'require'},
+        }
+    }
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -21,12 +61,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # apps do projeto
     'rachais',
     'accounts',
 ]
 
-# Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -57,14 +95,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'rachai.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
-# Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
@@ -72,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-# Auth
+
 AUTHENTICATION_BACKENDS = [
-    'accounts.backends.EmailOrUsernameModelBackend',  # autenticar por email (username=email)
+    'accounts.backends.EmailOrUsernameModelBackend',  
     'django.contrib.auth.backends.ModelBackend',
 ]
 
@@ -82,20 +113,19 @@ LOGIN_URL = 'accounts:login'
 LOGIN_REDIRECT_URL = 'rachais:group_list'
 LOGOUT_REDIRECT_URL = 'accounts:login'
 
-# I18N / TZ
 LANGUAGE_CODE = 'pt-br'
 TIME_ZONE = 'America/Recife'
 USE_I18N = True
 USE_TZ = True
 
-# Static files
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'assets',   # sua pasta de assets
+    BASE_DIR / 'assets',
 ]
 
-# Email (console)
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# Default PK field
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
